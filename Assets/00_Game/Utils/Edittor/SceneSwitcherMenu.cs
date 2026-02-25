36f91edfe81a64933cbff3fa9fe2401777954183
Dung <vuquocdung204@gmail.com>
2026-02-25 23:30:40 +0700

    upgrade BiRP to URP

--- Assets/00_Game/Utils/Edittor/SceneSwitcherMenu.cs
+++ Assets/00_Game/Utils/Edittor/SceneSwitcherMenu.cs
@@ -5,28 +5,26 @@ using UnityEditor.SceneManagement;
 
 public class SceneSwitcherMenu
 {
-    // Đường dẫn tới thư mục chứa các scene của bạn.
-    private const string SCENE_FOLDER_PATH = "Assets/Scenes/";
+    private const string SCENE_FOLDER_PATH = "Assets/00_Game/Scenes/";
 
     [MenuItem("Open Scene/Loading Scene", priority = 1)]
-    static void OpenLoadingScene()
+    static void OpenScene0()
     {
         OpenScene(SCENE_FOLDER_PATH + "LoadingScene.unity");
     }
 
     [MenuItem("Open Scene/Home Scene", priority = 2)]
-    static void OpenHomeScene()
+    static void OpenScene1()
     {
         OpenScene(SCENE_FOLDER_PATH + "HomeScene.unity");
     }
 
     [MenuItem("Open Scene/Game Play", priority = 3)]
-    static void OpenGameScene()
+    static void OpenScene2()
     {
         OpenScene(SCENE_FOLDER_PATH + "GamePlayScene.unity");
     }
-
-    // Hàm trợ giúp để mở scene và xử lý việc lưu các thay đổi
+    
     static void OpenScene(string scenePath)
     {
         if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
